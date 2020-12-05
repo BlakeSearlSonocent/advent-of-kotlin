cdd05d18a02591cdc2669e5e7176492f4d521ceb
Blake Searl <blake.searl@sonocent.com>
2020-12-05 10:04:34 +0000

    Clean up day 4 a bit

--- src/day4/Day4.kt
+++ src/day4/Day4.kt
@@ -13,40 +13,42 @@ class Passport {
             .toMap()
     }
 
-    fun partOne() = (creds.size == 8) || (creds.size == 7 && !creds.containsKey("cid"))
-    fun partTwo(): Boolean {
-        return partOne()
+    fun validPartOne() = (creds.size == 8) || (creds.size == 7 && !creds.containsKey("cid"))
+
+    fun validPartTwo(): Boolean {
+        return validPartOne()
                 && creds["byr"]?.toInt() in 1920..2002
                 && creds["iyr"]?.toInt() in 2010..2020
                 && creds["eyr"]?.toInt() in 2020..2030
-                && creds["hgt"]?.let { Regex("((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)").matches(it) }!!
-                && creds["hcl"]?.let { Regex("#([0-9]|[a-f]){6}").matches(it) }!!
+                && matchesRegex(creds["hgt"], Regex("((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)"))
+                && matchesRegex(creds["hcl"], Regex("#([0-9]|[a-f]){6}"))
                 && creds["ecl"].let { listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(it) }
-                && creds["pid"].let { Regex("[0-9]{9}").matches(it!!) }
+                && matchesRegex(creds["pid"], Regex("[0-9]{9}"))
     }
 }
 
+private fun matchesRegex(value: String?, regex: Regex): Boolean  = value?.let { regex.matches(it) } ?: false
+
 fun main() {
-    var elMapo = mapOf("a" to "b")
-    val truthy = elMapo["iyr"]?.let { Regex("abc").matches(it) }!!
-    print(truthy && true)
+    val lines = readFileToLines("src/day4/resources/Day4.txt")
+    val passports = readLinesToPassports(lines)
 
+    println("Part one: ${passports.count { it.validPartOne() }}")
+    println("Part two: ${passports.count { it.validPartTwo() }}")
+}
 
-//    val lines = readFileToLines("src/day4/resources/Day4.txt")
-//
-//    val passports = mutableListOf<Passport>()
-//    val cred = mutableListOf<String>()
-//    for (line in lines) {
-//        if (line.isEmpty()) {
-//            passports.add(Passport(cred))
-//            cred.clear()
-//        } else {
-//            cred.add(line)
-//        }
-//    }
-//
-//    passports.add(Passport(cred))
-//
-//    println("Part one: ${passports.count { it.partOne() }}")
-//    println("Part two: ${passports.count { it.partTwo() }}")
+private fun readLinesToPassports(lines: List<String>): MutableList<Passport> {
+    val passports = mutableListOf<Passport>()
+    val cred = mutableListOf<String>()
+    for (line in lines) {
+        if (line.isEmpty()) {
+            passports.add(Passport(cred))
+            cred.clear()
+        } else {
+            cred.add(line)
+        }
+    }
+
+    passports.add(Passport(cred))
+    return passports
 }
