b08cf3c73e1e8ac6af2fa4cc11a43cbd5e27f206
Blake Searl <blake.searl@sonocent.com>
2020-12-07 21:28:49 +0000

    Create a bag object and rename some methods

--- src/day7/Day7.kt
+++ src/day7/Day7.kt
@@ -5,20 +5,24 @@ import util.readFileToLines
 private val REGEX_LINES = """(\w+ \w+) bags contain (.*)""".toRegex()
 private val REGEX_BAG = """(\d+) (\w+ \w+) bags?""".toRegex()
 
-fun parse(lines: List<String>): Map<String, List<Pair<String, Int>>> {
+data class BagWithCount (val colour: String, val count: Int)
+
+fun parse(lines: List<String>): Map<String, List<BagWithCount>> {
     return lines.associate { l ->
         val (bag, containedBags) = REGEX_LINES.matchEntire(l)!!.destructured
         bag to REGEX_BAG.findAll(containedBags).map { containedBagAndCount ->
             val (count, colour) = containedBagAndCount.destructured
-            colour to count.toInt()
+            BagWithCount(colour, count.toInt())
         }.toList()
     }
 }
 
-fun getContainingBags (map: Map<String, List<Pair<String, Int>>>, containingBags: MutableList<String>, startBag: String, thisBag: String = startBag): Set<String> {
+
+
+fun getContainingBags (map: Map<String, List<BagWithCount>>, containingBags: MutableList<String>, startBag: String, thisBag: String = startBag): Set<String> {
     val containedBagsAndCounts = map[thisBag] ?: error("These shouldn't be null!")
 
-    containedBagsAndCounts.map { it.first }
+    containedBagsAndCounts.map { it.colour }
         .forEach { bag ->
             if (bag == "shiny gold") containingBags.add(startBag)
             else getContainingBags(map, containingBags, startBag, bag)
@@ -27,21 +31,19 @@ fun getContainingBags (map: Map<String, List<Pair<String, Int>>>, containingBags
     return containingBags.toSet()
 }
 
-fun getContainedBag (map: Map<String, List<Pair<String, Int>>>, thisBag: String): Int {
+fun getContainedBagCount (map: Map<String, List<BagWithCount>>, thisBag: String): Int {
     val containedBagsAndCounts = map[thisBag] ?: error("These shouldn't be null!")
 
-    return containedBagsAndCounts.sumBy { it.second * (1 + getContainedBag(map, it.first)) }
+    return containedBagsAndCounts.sumBy { it.count * (1 + getContainedBagCount(map, it.colour)) }
 }
 
 
 fun main() {
     val input = readFileToLines("src/day7/resources/Day7.txt")
-    val parsed = parse(input)
-    println(parsed.map { getContainingBags(parsed, mutableListOf(), it.key) }
-        .flatten()
-        .size)
+    val bagToContents = parse(input)
 
-    println(getContainedBag(parsed, "shiny gold"))
+    println("Part one: ${bagToContents.map { getContainingBags(bagToContents, mutableListOf(), it.key) }.flatten().size}")
+    println("Part two: ${getContainedBagCount(bagToContents, "shiny gold")}")
 }
 
 
