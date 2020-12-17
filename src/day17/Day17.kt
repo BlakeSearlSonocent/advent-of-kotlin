8065fd0bfdae8a68f99b19dd40e6f9826949fe53
Blake Searl <blake.searl@sonocent.com>
2020-12-17 10:32:48 +0000

    Day 17 part one

---
+++ src/day17/Day17.kt
@@ -0,0 +1,75 @@
+package day17
+
+import util.readFileToLines
+import kotlin.math.abs
+
+fun main() {
+    var alivePoints = readFileToLines("resources/Day17.txt").mapIndexed { yIndex, line ->
+        line.mapIndexed { xIndex, value ->
+            Point(
+                xIndex,
+                yIndex,
+                0
+            ) to value
+        }
+    }.flatten().filter { it.second == '#' }.toMap()
+
+    println(alivePoints)
+
+    for (i in 1..6) {
+        alivePoints = performCycle(alivePoints)
+    }
+
+    println(alivePoints.count())
+}
+
+fun performCycle(grid: Grid): MutableMap<Point, Char> {
+    val newGrid = mutableMapOf<Point, Char>()
+
+    val (x, y, z) = grid.getMaxDimensions()
+    for (ix in -x..x) {
+        for (iy in -y..y) {
+            for (iz in -z..z) {
+                val testPoint = Point(ix, iy, iz)
+                val c = grid[testPoint]
+                val neighbours = testPoint.getNeighbours()
+
+                val activeNeighbourCount = neighbours.map { neighbour -> neighbour to grid.getOrDefault(neighbour, '.') }
+                    .count { it.second == '#' }
+                if (c == null) {
+                    if (activeNeighbourCount == 3) newGrid[testPoint] = '#'
+                } else {
+                    if (activeNeighbourCount in listOf(2, 3)) newGrid[testPoint] = '#'
+                }
+            }
+        }
+    }
+
+    return newGrid
+}
+
+private fun Map<Point, Char>.getMaxDimensions(): Point {
+    val points = this.keys
+    return Point(abs(points.maxBy { abs(it.x) }!!.x) + 1,abs(points.maxBy { abs(it.y) }!!.y) + 1, abs(points.maxBy { abs(it.z) }!!.z) + 1)
+}
+
+
+typealias Grid = Map<Point, Char>
+
+data class Point(val x: Int, val y: Int, val z: Int) {
+    fun getNeighbours(): List<Point> {
+        val neighbours = mutableListOf<Point>()
+        val perturbations = listOf(-1, 0, 1)
+
+        for (xPerturb in perturbations) {
+            for (yPerturb in perturbations) {
+                for (zPerturb in perturbations) {
+                    val potentialNeighbour = Point(x + xPerturb, y + yPerturb, z + zPerturb)
+                    if (potentialNeighbour != this) neighbours.add(potentialNeighbour)
+                }
+            }
+        }
+
+        return neighbours
+    }
+}
