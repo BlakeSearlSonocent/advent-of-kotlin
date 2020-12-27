4056c2a6200dbe76d786af3904ecd21a20bcf682
Blake Searl <blake.searl@sonocent.com>
2020-12-27 13:58:05 +0000

    Day 25

---
+++ src/day25/Day25.kt
@@ -0,0 +1,16 @@
+package day25
+
+fun main() {
+    var a = 1L
+    var b = 1L
+
+    while (true) {
+        a = (a * 7) % 20201227L
+        b = (b * 18356117) % 20201227L
+
+        if (a == 5909654L) {
+            return println(b)
+            break
+        }
+    }
+}
