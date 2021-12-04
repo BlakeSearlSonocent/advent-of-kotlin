6e70a8813b46f40b3213fce121b973dcdead0959
Blake Searl <blake.searl@sonocent.com>
2021-12-04 13:04:12 +0000

    Some tidying

--- src/aoc/2021/four.kt
+++ src/aoc/2021/four.kt
@@ -5,50 +5,52 @@ import util.readDoubleLineSeparatedFileToGroups
 fun main() {
     val input = readDoubleLineSeparatedFileToGroups("2021.4.txt")
     val calls = input[0].split(",").map { it.toInt() }
-    println(calls)
 
     val cards = input.drop(1).map { card ->
         val stringLines = card.split("\n").map { it.trim() }
-        BingoCard(stringLines.map { line -> line.split("\\s+".toRegex()).map { it.toInt() to false } })
+        BingoCard(stringLines.map { line -> line.split("\\s+".toRegex()).map { BingoSquare(it.toInt()) } })
     }
 
-//    for (call in calls) {
-//        if (cards.map { card -> card.updateAndCheck(call) }.any { it }) {
-//            println("Done")
-//            break
-//        }
-//    }
+    for (call in calls) {
+        val completedCard =
+            cards.find { card -> card.updateAndCheckCompletion(call) }?.run { println("Part one: ${sumCard() * call}") }
+        if (completedCard != null) break
+    }
+
+    cards.forEach { it.reset() }
 
     val completedCards = mutableListOf<Int>()
     for (call in calls) {
         val newlyCompleted =
-            cards.withIndex().filter { card -> card.index !in completedCards && card.value.updateAndCheck(call) }
+            cards.withIndex()
+                .filter { card -> card.index !in completedCards && card.value.updateAndCheckCompletion(call) }
                 .map { it.index }
 
         completedCards += newlyCompleted
 
         if (completedCards.size == cards.size) {
-            println(cards[newlyCompleted.first()].sumCard() * call)
+            println("Part two: ${cards[newlyCompleted.first()].sumCard() * call}")
             break
         }
     }
 }
 
-data class BingoCard(var rows: List<List<Pair<Int, Boolean>>>) {
-    fun updateAndCheck(call: Int): Boolean {
-        rows = rows.map { row -> row.map { if (it.first == call) Pair(it.first, true) else it } }
+data class BingoCard(val rows: List<List<BingoSquare>>) {
+    fun updateAndCheckCompletion(call: Int): Boolean {
+        rows.forEach { row -> row.forEach { it.completeIfMatch(call) } }
 
-        val completedRows = rows.count { row -> row.count { it.second } == 5 }
-        val completedColumns = (0..4).count { index -> rows.count { it[index].second } == 5 }
+        val completedRows = rows.count { row -> row.count { it.checked } == 5 }
+        val completedColumns = (0..4).count { index -> rows.count { it[index].checked } == 5 }
 
-        if (completedRows > 0 || completedColumns > 0) {
-            println(sumCard())
-
-            return true
-        }
-
-        return false
+        return completedRows + completedColumns > 0
     }
 
-    fun sumCard() = rows.sumOf { row -> row.sumOf { if (!it.second) it.first else 0 } }
+    fun sumCard() = rows.sumOf { row -> row.filterNot { it.checked }.sumOf { it.number } }
+    fun reset() = rows.forEach { it.forEach { square -> square.checked = false } }
+}
+
+data class BingoSquare(val number: Int, var checked: Boolean = false) {
+    fun completeIfMatch(call: Int) {
+        if (number == call) checked = true
+    }
 }
