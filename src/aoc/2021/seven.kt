673a29246b2ee37d6d5e52e601a9cf1f6f920db4
Blake Searl <blake.searl@sonocent.com>
2021-12-09 18:48:42 +0000

    Simplify day 7

--- src/aoc/2021/seven.kt
+++ src/aoc/2021/seven.kt
@@ -5,32 +5,11 @@ import kotlin.math.abs
 
 fun main() {
     val positions = readFileToText("2021.7.txt").split(",").map { it.toInt() }
-    val distances = mutableListOf<Int>()
-    for (position in positions) {
-        distances += positions.sumOf { abs(it - position) }
-    }
+    println(positions.map { position -> positions.sumOf { abs(it - position) } }.minOf { it })
 
-    println(distances.minOf { it })
-
-    val fuelCosts = mutableListOf<Int>()
     val min = positions.minOf { it }
     val max = positions.maxOf { it }
-    for (position in min..max) {
-        fuelCosts += positions.sumOf { fuelCost(it, position) }
-    }
-
-    println(fuelCosts.minOf { it })
+    println((min..max).map { positions.sumOf { position -> fuelCost(it, position) } }.minOf { it })
 }
 
-fun fuelCost(start: Int, finish: Int): Int {
-    var fuelCost = 0
-    if (finish == start) {
-        return fuelCost
-    }
-
-    return if (finish > start) {
-        fuelCost + finish - start + fuelCost(start + 1, finish)
-    } else {
-        fuelCost + start - finish + fuelCost(start, finish + 1)
-    }
-}
+fun fuelCost(start: Int, finish: Int) = (1..abs(finish - start)).sumOf { it }
