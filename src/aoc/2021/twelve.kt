448c2414e4bc10b3deca64b2d83477efb17cf42d
Blake Searl <blake.searl@sonocent.com>
2021-12-12 15:57:36 +0000

    Day 12

---
+++ src/aoc/2021/twelve.kt
@@ -0,0 +1,59 @@
+package aoc.`2021`
+
+import util.readLines
+
+fun main() {
+    val joins = readLines("2021.12.txt").map { it.split("-") }
+        .filter { (first, second) -> first !== "end" && second !== "start" }.map { it[0] to it[1] }
+
+    val neighbours = mutableMapOf<String, Set<String>>()
+    for ((first, second) in joins) {
+        neighbours[first] = neighbours.getOrDefault(first, emptySet()) + second
+        neighbours[second] = neighbours.getOrDefault(second, emptySet()) + first
+    }
+
+    val caveSystem = CaveSystem(neighbours)
+    caveSystem.partOne()
+    caveSystem.partTwo()
+}
+
+class CaveSystem(private val system: Map<String, Set<String>>) {
+    private fun satisfiesPartTwoRule(cave: String, currentPath: List<String>) =
+        cave !in currentPath || currentPath.filter { it.lowercase() == it }.groupingBy { it }.eachCount()
+            .none { it.value > 1 }
+
+    private fun satisfiesPartOneRule(cave: String, currentPath: List<String>) = cave !in currentPath
+
+    private fun populatePaths(
+        start: String,
+        currentPath: List<String>,
+        paths: MutableSet<List<String>>,
+        passesSmallCaveRule: (cave: String, currentPath: List<String>) -> Boolean
+    ) {
+        when {
+            start == "end" -> {
+                val finalPath = currentPath + start
+                paths += finalPath
+            }
+            (start.lowercase() == start && passesSmallCaveRule(start, currentPath)) || start.uppercase() == start -> {
+                val updatedPath = currentPath + start
+                val nextCaves = system.getValue(start).filterNot { it == "start" }
+                for (cave in nextCaves) {
+                    populatePaths(cave, updatedPath, paths, passesSmallCaveRule)
+                }
+            }
+        }
+    }
+
+    fun partOne() {
+        val paths = mutableSetOf<List<String>>()
+        populatePaths("start", mutableListOf(), paths, ::satisfiesPartOneRule)
+        println("Part one: ${paths.size}")
+    }
+
+    fun partTwo() {
+        val paths = mutableSetOf<List<String>>()
+        populatePaths("start", mutableListOf(), paths, ::satisfiesPartTwoRule)
+        println("Part two: ${paths.size}")
+    }
+}
