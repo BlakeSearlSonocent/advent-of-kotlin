c88ea57c8a43a19a093aae431069d9d06c425793
Blake Searl <blake.searl@sonocent.com>
2021-12-12 19:19:46 +0000

    More day 12 tidy

--- src/aoc/2021/twelve.kt
+++ src/aoc/2021/twelve.kt
@@ -2,58 +2,46 @@ package aoc.`2021`
 
 import util.readLines
 
-fun main() {
-    val joins = readLines("2021.12.txt").map { it.split("-") }.map { it[0] to it[1] }
+typealias Cave = String
 
-    val neighbours = mutableMapOf<String, Set<String>>()
-    for ((first, second) in joins) {
-        neighbours[first] = neighbours.getOrDefault(first, emptySet()) + second
-        neighbours[second] = neighbours.getOrDefault(second, emptySet()) + first
-    }
+fun Cave.isMinorCave() = this.lowercase() == this
+fun Cave.isMajorCave() = !this.isMinorCave()
+
+fun main() {
+    val neighbours = readLines("2021.12.txt").map { it.split("-") }
+        .fold(mutableMapOf<Cave, Set<Cave>>()) { acc, (first, second) ->
+            acc[first] = acc.getOrDefault(first, emptySet()) + second
+            acc[second] = acc.getOrDefault(second, emptySet()) + first
+            acc
+        }.toMap()
 
     val caveSystem = CaveSystem(neighbours)
-    caveSystem.partOne()
-    caveSystem.partTwo()
+    println("Part one: ${caveSystem.partOne().size}")
+    println("Part two: ${caveSystem.partTwo().size}")
 }
 
-class CaveSystem(private val system: Map<String, Set<String>>) {
-    private fun satisfiesPartTwoRule(cave: String, currentPath: List<String>) =
-        cave !in currentPath || currentPath.filter { it.lowercase() == it }.groupingBy { it }.eachCount()
+class CaveSystem(private val system: Map<Cave, Set<Cave>>) {
+    private fun satisfiesPartTwoRule(cave: Cave, currentPath: List<Cave>) =
+        cave !in currentPath || currentPath.filter { it.isMinorCave() }.groupingBy { it }.eachCount()
             .none { it.value > 1 }
 
-    private fun satisfiesPartOneRule(cave: String, currentPath: List<String>) = cave !in currentPath
+    private fun satisfiesPartOneRule(cave: Cave, currentPath: List<Cave>) = cave !in currentPath
 
     private fun populatePaths(
-        start: String,
-        currentPath: List<String>,
-        paths: MutableSet<List<String>>,
-        passesSmallCaveRule: (cave: String, currentPath: List<String>) -> Boolean
-    ) {
+        passesSmallCaveRule: (cave: Cave, currentPath: List<Cave>) -> Boolean,
+        start: Cave = "start",
+        currentPath: List<Cave> = emptyList()
+    ): List<List<Cave>> {
         if (start == "end") {
-            val finalPath = currentPath + start
-            paths += finalPath
-        } else if ((
-            start.lowercase() == start && passesSmallCaveRule(
-                    start,
-                    currentPath
-                )
-            ) || start.uppercase() == start
-        ) {
-            val updatedPath = currentPath + start
-            system.getValue(start).filterNot { it == "start" }
-                .forEach { populatePaths(it, updatedPath, paths, passesSmallCaveRule) }
+            return listOf(currentPath + start)
+        } else if ((start.isMinorCave() && passesSmallCaveRule(start, currentPath)) || start.isMajorCave()) {
+            return system.getValue(start).filterNot { it == "start" }
+                .flatMap { populatePaths(passesSmallCaveRule, it, currentPath + start) }
         }
+
+        return emptyList()
     }
 
-    fun partOne() {
-        val paths = mutableSetOf<List<String>>()
-        populatePaths("start", mutableListOf(), paths, ::satisfiesPartOneRule)
-        println("Part one: ${paths.size}")
-    }
-
-    fun partTwo() {
-        val paths = mutableSetOf<List<String>>()
-        populatePaths("start", mutableListOf(), paths, ::satisfiesPartTwoRule)
-        println("Part two: ${paths.size}")
-    }
+    fun partOne() = populatePaths(::satisfiesPartOneRule)
+    fun partTwo() = populatePaths(::satisfiesPartTwoRule)
 }
